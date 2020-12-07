92e6e603e81bfe7601e4a8afda06f64e53af97f3
Mark Crossfield <mark.crossfield@autotrader.co.uk>
2020-12-07 19:08:26 +0000

    Parse strings as binary

--- day5.ts
+++ day5.ts
@@ -1,25 +1,13 @@
 let input: string = 'FBFBBFFRLR';
 
-let seatNumber = 0;
-for (let seatIndex: number = 0, power: number = 6; seatIndex <= 7; seatIndex++, power--) {
-    if (/B/.test(input.charAt(seatIndex))) seatNumber += 2 ** power;
-
+function seatRow(seatCode:string) {
+    return parseInt(seatCode.slice(0,7).split('').map(c => c == 'B' ? 1 : 0).join(''),2)
 }
-function decodeBinary(value : string) {
-    if (/^$/.test(value)) return 0;
-    let charValue: number = 0
-    if (/B/.test(value.charAt(0))) charValue = 2 ** (value.length-1);
-    return charValue + decodeBinary(value.slice(1))
+function seatCol(seatCode:string) {
+    return parseInt(seatCode.slice(7,10).split('').map(c => c == 'R' ? 1 : 0).join(''),2)
 }
-console.log(decodeBinary('FBFBBFF'))
-console.log(parseInt('0101100',2))
-let converted = 'FBFBBFF'.split('').map(c => c=='B' ? 1 : 0).join('');
-console.log(parseInt(converted,2))
-// input.slice(0,7).split('').forEach(character => {
-//     console.log(character)
-//     if (/F/.test(character)) {
-//         seatNumber = seatNumber >> 1;
-//     }
-//     console.log(seatNumber)
-// })
-console.log(seatNumber)
+function seatID(seatCode:string) {
+    return seatRow(seatCode) * 8 + seatCol(seatCode);
+}
+console.log(seatRow('FBFBBFFRLR'))
+console.log(seatCol('FBFBBFFRLR'))
